ee5992c1541f0a532b13c64516d11d080e9d8ba6
Oliver C <ojc011@yahoo.com>
2022-07-19 15:33:32 -0500

    Created Vote model to populate votes on a post. Like comments, vote also includes a dynamic property that will delete it from the database if the corresponding post is also removed.

--- seeds.py
+++ seeds.py
@@ -1,4 +1,4 @@
-from app.models import User, Post, Comment
+from app.models import User, Post, Comment, Vote
 from app.db import Session, Base, engine
 
 
@@ -37,6 +37,17 @@ db.add_all([ #preps SQL queries
   Comment(comment_text='Aliquam erat volutpat. In congue.', user_id=2, post_id=1),
   Comment(comment_text='Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.', user_id=2, post_id=3),
   Comment(comment_text='In hac habitasse platea dictumst.', user_id=3, post_id=3)
+])
+
+db.commit() #INSERT statement into db
+
+# insert votes
+db.add_all([ #preps SQL queries
+  Vote(user_id=1, post_id=2),
+  Vote(user_id=1, post_id=4),
+  Vote(user_id=2, post_id=4),
+  Vote(user_id=3, post_id=4),
+  Vote(user_id=4, post_id=2)
 ])
 
 db.commit() #INSERT statement into db
