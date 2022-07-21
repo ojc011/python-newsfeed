8635ab8bf18f58a05e1dd105ea71074a984603f9
Oliver C <ojc011@yahoo.com>
2022-07-21 16:24:28 -0500

    Added error handling by inserting try...except statement into api route. Will return error message if validations or sql criteria are not met and then rollback the db if the dbcommit fails so that pending connections do not pile up and crash the app

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,3 +1,4 @@
+import sys
 from flask import Blueprint, request, jsonify
 from app.models import User
 from app.db import get_db
@@ -9,15 +10,21 @@ def signup():
   data = request.get_json()
   db = get_db()
 
-  # create a new user
-  newUser = User(
-    username = data['username'],    # js version would look like 'username = data.username,
-    email = data['email'],          #                            'email = data.email,
-    password = data['password']     #                            'password = data.password
-  )
+  try:
+    # create a new user
+    newUser = User(
+        username = data['username'],    # js version would look like 'username = data.username,
+        email = data['email'],          #                            'email = data.email,
+        password = data['password']     #                            'password = data.password
+    )
 
-  # save in database
-  db.add(newUser) # preps db to insert newUser into database
-  db.commit()
+    # save in database
+    db.add(newUser) # preps db to insert newUser into database
+    db.commit()
+  except: #insert of data failed, error will be sent to frontend
+    print(sys.exc_info()[0])
+    
+    db.rollback()
+    return jsonify(message = 'Signup has failed'), 500
 
   return jsonify(id = newUser.id) # includes id of newUser 
